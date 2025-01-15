c4c24ab49e23cf72f14b31717fe5d5924a116d11
Ingaiza <alvindavid898@gmail.com>
2025-01-15 20:32:11 +0300

    added respawn to module_server

--- init/launch/aimbot.launch.py
+++ init/launch/aimbot.launch.py
@@ -6,21 +6,20 @@ from ament_index_python.packages import get_package_share_path
 from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
-
     imu_node = Node(
         package="wit_imu_driver",
         executable='wit_imu_node',
         output='screen',
         parameters=[{
             'orientation_covariance': [0.01, 0, 0,
-                                        0, 0.01, 0,
-                                        0, 0, 0.01],
+                                     0, 0.01, 0,
+                                     0, 0, 0.01],
             'angular_velocity_covariance': [0.01, 0, 0,
-                                            0, 0.01, 0,
-                                            0, 0, 0.01],
+                                          0, 0.01, 0,
+                                          0, 0, 0.01],
             'linear_acceleration_covariance': [0.1, 0, 0,
-                                                0, 0.1, 0,
-                                                0, 0, 0.1]
+                                             0, 0.1, 0,
+                                             0, 0, 0.1]
         }]
     )
 
@@ -33,10 +32,10 @@ def generate_launch_description():
     module_server = Node(
         package="yrm100",
         executable="module_server",
-        output="screen"
+        output="screen",
+        respawn=True,  
+        respawn_delay=1.0  
     )
-
-   
 
     return LaunchDescription([
         imu_node,
